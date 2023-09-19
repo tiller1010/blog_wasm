ba336a9cbda79003a967a1f725502a0d770fba5f
Tyler Trout <tiller1010@gmail.com>
2023-09-18 22:39:33 -0400

    feat: working wasm ajax

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,33 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
 #[wasm_bindgen]
-//pub async fn get_page_content(url: &str) -> Result<JsValue, JsValue> {
-//    let promise = js_sys::Promise::resolve(&42.into());
+pub async fn get_page_content(url: &str) -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    request
+        .headers()
+        .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")?;
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    let html = JsFuture::from(resp.text()?).await?;
+
+    Ok(html)
+
+//    let js_url = JsValue::from_str(url);
+//    let promise = js_sys::Promise::resolve(&js_url);
 //    let result = wasm_bindgen_futures::JsFuture::from(promise).await?;
 //    Ok(result)
-//}
+}
 
 #[wasm_bindgen]
 extern "C" {
